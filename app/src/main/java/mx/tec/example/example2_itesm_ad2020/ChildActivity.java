826ba3a331d64721d1f0e7c6179fd1d60521cdab
maualvm@gmail.com <maualvm@gmail.com>
2020-08-27 20:17:36 -0500

    Agregar botón de regreso de ChildActivity a MainActivity

--- app/src/main/java/mx/tec/example/example2_itesm_ad2020/ChildActivity.java
+++ app/src/main/java/mx/tec/example/example2_itesm_ad2020/ChildActivity.java
@@ -2,13 +2,28 @@ package mx.tec.example.example2_itesm_ad2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChildActivity extends AppCompatActivity {
+
+    Button goBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
+
+        goBack = findViewById(R.id.buttonBack);
+
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(ChildActivity.this, MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
